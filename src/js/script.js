1eb90886e37826a50c9646b48f6e4219e6b8970d
Anne-Marie Dufour <hello@annemariedufour.com>
2020-05-21 06:37:23 +0200

    Remove swap index from impact column

--- src/js/script.js
+++ src/js/script.js
@@ -588,7 +588,6 @@ function updateSimulation() {
   d3.select('.index-circle')
     .attr('r', radiusIndex * foodprintAreaFactor * 0.9);
   animateNumber('foodprint-result--number--index', +indexCounter.innerHTML, foodprintIndex);
-  animateNumber('impact-index--number', +indexCounter.innerHTML, foodprintIndex);
 
   // If swap, show swap impact tooltip
   if (isSwap && currentSwap.key !== 'reset') {
