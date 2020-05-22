953790783fcb76cb8af1eef08db6ae8399305416
Anne-Marie Dufour <hello@annemariedufour.com>
2020-05-22 18:24:01 +0200

    Ensure that countdown animation is performed within 100 increments

--- src/js/animateNumbers.js
+++ src/js/animateNumbers.js
@@ -4,10 +4,9 @@ function animateNumber(id, count, target) {
   const counter = document.getElementById(id);
   let factor;
   factor = count < target ? 1 : -1;
+  const increment = factor * Math.abs(target - count) / speed;
 
   const updateCount = () => {
-    const increment = factor * target / speed;
-
     if ((factor > 0 && count < target) || (factor < 0 && count > target)) {
       count += increment;
       counter.innerHTML = (count).toFixed(2);
